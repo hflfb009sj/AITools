180c4868d050e85f485202bc5018474a162db1a5
hflfb009sj <riahig45@gmail.com>
2026-04-24 02:14:12 +0100

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,12 +2,13 @@ import type { Metadata } from "next";
 import "./globals.css";
 import Navbar from "@/components/layout/Navbar";
 import Footer from "@/components/layout/Footer";
+import Script from "next/script";
 
 export const metadata: Metadata = {
   title: "Votre Titre",
   description: "Votre description",
   verification: {
-    google: "h3qV_hHh2pPZXWANoOrALWU_kCmQAzCklejUUKNWraQ", // Copiez uniquement la valeur ici
+    google: "h3qV_hHh2pPZXWANoOrALWU_kCmQAzCklejUUKNWraQ",
   },
 };
 
@@ -19,6 +20,20 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
       </head>
       <body>
+        {/* Google Analytics */}
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-030CWS3CQY"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-030CWS3CQY');
+          `}
+        </Script>
+
         <Navbar />
         <main>{children}</main>
         <Footer />
